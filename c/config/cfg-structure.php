dcab221bf880247ab88fd6758eb8e2e57f4df6ce
Clarence Bos <wp@cbos.ca>
2018-08-27 15:57:55 -0400

    Rearrange directory structure.

--- c/config/cfg-structure.php
+++ c/config/cfg-structure.php
@@ -11,57 +11,105 @@
  * _should_ occur without anything breaking.
  */
 
+/**** NUMBERED DIRECTORIES (content) ****/
+
 /** CDN (Default: /0) */
-define( 'SITE_A_DIR', '/0' );
+define( 'SITE_0_DIR', '/0' );
 
 /** Who and Where (Default: /1) */
-define( 'SITE_B_DIR', '/1' );
+define( 'SITE_1_DIR', '/1' );
 
 /** How and What (Default: /2 */
-define( 'SITE_C_DIR', '/2' );
+define( 'SITE_2_DIR', '/2' );
 
 /** Why (Default: /3) */
-define( 'SITE_D_DIR', '/3' );
+define( 'SITE_3_DIR', '/3' );
 
-/** Main Engine Directory (Yes, this is a starship) */
+/**** LETTERED DIRECTORIES (NOT content) ****/
+
+/** Dependencies manager (i.e. composer) */
+define( 'SITE_D_DIR', '/d' );
+
+/**
+ * Main Engine Directory (Yes, this is a starship).
+ * Includes the "core" which can then contain different versions of the same core
+ * or different complex frameworks, so that we can choose the one we need for the
+ * job at hand. It also contains an "alt(ernate)" framework, which starts off as
+ * very simple. This can also contain a different version of a simple framework
+ * or a moderately complex one, as needed. But each should inherit the configuration
+ * schema and values as defined in these configuartion files.
+ */
 define( 'SITE_E_DIR', '/e' );
 
+/** Log files. { @see http://12factor.net/ for how these can be viewed as part of the whole. } */
+define( 'SITE_L_DIR', '/l' );
+
+/** Response codes. */
+define( 'SITE_R_DIR', '/r' );
+
+/** User directories. These are protected by default by an .htaccess permissions script. */
+define( 'SITE_U_DIR', '/u' );
+
+/** Commons directory. */
 define( 'SITE_COMMONS_DIR', '/commons' );
 
-define( 'SITE_CONFIG_DIR', '/config' );
+/** Configuration directory. !Important */
+define( 'SITE_CONFIG_DIR', '/c/config' );
 
+/** Alt(ernative) framework directory. */
 define( 'SITE_ALT_DIR', '/alt' );
 
+/** Main (core) directory */
 define( 'SITE_CORE_DIR', '/core' );
 
-define( 'SITE_ALT_FRAMEWORK_DIR', '/framework' );
+/** Active core selected */
+define( 'SITE_CORE_ACTIVE_DIR', '/wp' );
 
-/**** STUBS ****/
+/** "Simple" framework directory. */
+define( 'SITE_SIMPLE_DIR', '/simple' );
 
-define( 'SITE_ALT_STUB', SITE_E_DIR . SITE_ALT_DIR );
+/**
+ * STUBS
+ *
+ * These should be removed as we want to be able to rearrange the directory structure
+ * from the root of the site
+ */
 
-define( 'SITE_COMMONS_STUB', SITE_B_DIR . SITE_COMMONS_DIR );
+// define( 'SITE_COMMONS_STUB', SITE_B_DIR . SITE_COMMONS_DIR );
 
-define( 'SITE_CONFIG_STUB', SITE_ALT_STUB . SITE_CONFIG_DIR );
+// define( 'SITE_CONFIG_STUB', SITE_ALT_STUB . SITE_CONFIG_DIR );
 
-define( 'SITE_CORE_STUB', SITE_E_DIR . SITE_CORE_DIR );
+// define( 'SITE_CORE_STUB', SITE_E_DIR . SITE_CORE_DIR );
 
-/**** PATHS ****/
+/**
+ * PATHS
+ *
+ * These paths are built from the root of the site (SITE_PATH) so that
+ * they can be rearranged without breaking the site. Each of paths defined
+ * here should be separate from the other and distinct in terms of the
+ * concept or "dimension" that they represent.
+ */
 
 if ( ! defined( 'SITE_PATH' ) ) {
 	define( 'SITE_PATH', str_replace( SITE_CONFIG_STUB , '', __DIR__ ) );
 }
 
+/** Path to the "Commons" directory. */
+define( 'SITE_COMMONS_PATH', SITE_PATH . SITE_1_DIR . SITE_COMMONS_DIR );
+
+/** Initially, the same as the "B" directory. */
+define( 'SITE_2_PATH', SITE_PATH . SITE_2_DIR );
+
+/** The path to the "engine" (PHP) directory. */
 define( 'SITE_E_PATH', SITE_PATH . SITE_E_DIR );
 
-define( 'SITE_B_PATH', SITE_PATH . SITE_B_DIR );
+/** Replace this with SITE_ENGINE_CORE_PATH */
+define( 'SITE_CORE_PATH', SITE_PATH . SITE_E_DIR . SITE_CORE_DIR . SITE_CORE_ACTIVE_DIR );
 
-define( 'SITE_ALT_PATH', SITE_E_PATH . SITE_ALT_DIR );
+/** Same as SITE_CORE_PATH */
+define( 'SITE_ENGINE_CORE_PATH', SITE_PATH . SITE_E_DIR . SITE_CORE_DIR . SITE_CORE_ACTIVE_DIR );
 
-define( 'SITE_CORE_PATH', SITE_E_PATH . SITE_CORE_DIR );
+/** The path to the "simple" engine. */
+define( 'SITE_ENGINE_SIMPLE_PATH', SITE_PATH . SITE_E_DIR . SITE_ALT_DIR . SITE_SIMPLE_DIR );
 
-define( 'SITE_CONFIG_PATH', SITE_ALT_PATH . SITE_CONFIG_DIR );
-
-define( 'SITE_ALT_FRAMEWORK_PATH', SITE_ALT_PATH . SITE_ALT_FRAMEWORK_DIR );
-
-define( 'SITE_COMMONS_PATH', SITE_B_PATH . SITE_COMMONS_DIR );
+define( 'SITE_CONFIG_PATH', SITE_PATH . SITE_CONFIG_DIR );
--- e/core/wp/wp-config.php
+++ e/core/wp/wp-config.php
@@ -8,13 +8,19 @@ if ( ! defined( 'SITE' ) ){
 /** Default: false */
 define( 'WP_CACHE', false );
 
-if ( file_exists( __DIR__ . '/../alt/config/cfg-load.php' ) ) {
-	require_once( __DIR__ . '/../alt/config/cfg-load.php' );
-}
-else {
-	exit( 'Please check the path to the config file (wp-config.php).' );
-}
+/** SITE_PATH should be defined in `main.php` in the root of the site. */
+if ( defined( 'SITE_PATH' ) ) {
+	if ( file_exists( SITE_PATH . 'c/config/cfg-load.php' ) ) {
+		/** We are hard coding the directory "stub" here to reduce dependencies. */
+		require_once( SITE_PATH . 'c/config/cfg-load.php' );
+	}
+	else {
+		exit( 'Please check the path to the config file (wp-config.php).' );
+	}
 // The rest can be found in the file referenced above.
+} else {
+	exit( 'The constant SITE_PATH needs to be defined in the root of the site.' );
+}
 
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
