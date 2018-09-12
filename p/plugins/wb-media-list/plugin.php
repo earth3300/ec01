db1f6082eefe0ed62d9f127e5d39050e777a135d
Clarence Bos <wp@cbos.ca>
2018-09-12 18:19:10 -0400

    Improve.

--- p/plugins/wb-media-list/plugin.php
+++ p/plugins/wb-media-list/plugin.php
@@ -40,27 +40,219 @@ class MediaList
 	 */
 	public function get( $args )
 	{
-		$max = isset( $args['max'] ) ? $args['max'] : 5;
-		$root = SITE_CDN_PATH;
-		$pattern = "/*.jpg";
+		$max = $this->get_max_images( $args );
 
-		$match =  $root . $args['dir'] . $pattern;
-
-		$str = '<article>' . PHP_EOL;
-		$cnt = 0;
-		foreach ( glob( $match ) as $file )
+		if ( $this->check_args( $args ) )
 		{
-			$cnt++;
-			if ( $cnt > $max )
+			$match = $this->match( $args );
+			$str = '<article>' . PHP_EOL;
+			$cnt = 0;
+			foreach ( glob( $match ) as $file )
 			{
-				break;
+				$cnt++;
+				if ( $cnt > $max )
+				{
+					break;
+				}
+				/** Remove the root of the file path to use it an image source. */
+				$args['src'] = str_replace( SITE_PATH, '', $file );
+				$str .= $this->get_image_html( $args );
 			}
-			/** Remove the root of the file path to use it an image source. */
-			$src = str_replace( SITE_PATH, '', $file );
-			$str .= sprintf('<a href="%s"><img src="%s" width="500" height="auto" alt="Image" class="generic" /></a>%s', SITE_URL . $src, $src, PHP_EOL );
+			$str .= '</article>' . PHP_EOL;
+			return $str;
 		}
-		$str .= '</article>' . PHP_EOL;
+		else
+		{
+			return "Error.";
+		}
+	}
+
+	/**
+	 * Get the maximum number of images to process..
+	 *
+	 * @param array $args
+	 * @return int
+	 */
+	private function get_max_images( $args )
+	{
+		if ( isset( $args['max'] ) )
+		{
+			$max = $args['max'];
+		}
+		else
+		{
+			$max = 5;
+		}
+		return $max;
+	}
+
+	/**
+	 * Build the match string.
+	 *
+	 * @param array $args
+	 * @return string|false
+	 */
+	private function match( $args )
+	{
+		if (  defined( 'SITE_CDN_PATH' ) ) {
+			$root = SITE_CDN_PATH;
+			$prefix = "/*";
+			$type = $this -> get_image_type( $args );
+			$pattern =  $prefix . $type;
+			$match =  $root . $args['dir'] . $pattern;
+			return $match;
+		}
+		else {
+			return "Error.";
+		}
+	}
+
+	/**
+	 * Get the type of image to process.
+	 *
+	 * If no type is specified, return the default (.jpg). Include the dot (.)
+	 * for simplicity.
+	 *
+	 * jpg or png. Default: jpg
+	 *
+	 * @param array $args
+	 * @return string
+	 */
+	private function get_image_type( $args )
+	{
+		if ( isset( $args['type'] ) )
+		{
+			if ( 'png' == $args['type'] )
+			{
+				return '.png';
+			}
+			else if ( 'jpg' == $args['type'] )
+			{
+				return '.jpg';
+			}
+		}
+		else
+		{
+			return '.jpg';
+		}
+	}
+
+	/**
+	 * Get the image HTML
+	 *
+	 * If no type is specified, return the default (.jpg). Include the dot (.)
+	 * for simplicity.
+	 *
+	 * jpg or png. Default: jpg
+	 *
+	 * @param array $args
+	 * @return string
+	 */
+	private function get_image_html( $args )
+	{
+		$str = sprintf( '<a href="%s">', SITE_URL . $args['src'] );
+		$str .= '<img ';
+		$str .= sprintf( ' class="%s"', $this->get_image_class( $args ) );
+		$str .= sprintf( ' src="%s"', $args['src'] );
+		$str .= sprintf( ' alt="%s"', $this->get_image_alt( $args ) );
+		$str .= sprintf( ' width="600"', $this->get_image_width( $args ) );
+		$str .= sprintf( ' height="%s"', $this->get_image_height( $args ) );
+		$str .= ' />';
+		$str .= '</a>' . PHP_EOL;
 		return $str;
+	}
+
+	/**
+	 * Get the image width.
+	 *
+	 * @param array $args
+	 * @return string
+	 */
+	private function get_image_width( $args )
+	{
+		if ( defined( 'SITE_IMAGE_WIDTH' ) )
+		{
+			$width = SITE_IMAGE_WIDTH;
+		}
+		else
+		{
+			$width = '600';
+		}
+	}
+
+	/**
+	 * Get the image height.
+	 *
+	 * @param array $args
+	 * @return string
+	 */
+	private function get_image_height( $args )
+	{
+		if ( defined( 'SITE_IMAGE_HEIGHT' ) )
+		{
+			$height = SITE_IMAGE_HEIGHT;
+		}
+		else
+		{
+			$height = '400';
+		}
+		return $height;
+	}
+
+/**
+	 * Get the image alt tag.
+	 *
+	 * @param array $args
+	 * @return string
+	 */
+	private function get_image_alt( $args )
+	{
+		if ( "" !== $args['src'] )
+		{
+			$alt = $args['src'];
+		}
+		else
+		{
+			$alt = "Not available";
+		}
+		return $alt;
+	}
+
+	/**
+	 * Get the image class.
+	 *
+	 * @param array $args
+	 * @return string
+	 */
+	private function get_image_class( $args )
+	{
+		if ( defined( 'SITE_IMAGE_CLASS' ) )
+		{
+			$class = SITE_IMAGE_CLASS;
+		}
+		else
+		{
+			$class = 'generic';
+		}
+		return $class;
+	}
+
+	/**
+	 * Check the arguments from the shortcode
+	 *
+	 * @param array $args
+	 * @return bool
+	 */
+	private function check_args( $args )
+	{
+		if ( isset( $args['dir'] ) )
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 }
 
@@ -75,13 +267,15 @@ class MediaList
  */
 function media_list( $args )
 {
-	if ( ! is_array( $args ) || ! isset( $args['dir'] ) ) {
-		return '<!-- Missing directory. We need this. -->';
+	if ( is_array( $args ) )
+	{
+		$media_list = new MediaList();
+		return $media_list -> get( $args );
 	}
-
-	$media_list = new MediaList();
-
-    return $media_list -> get( $args );
+	else
+	{
+		return '<!-- Missing the image directory to process. [media-list dir=""]-->';
+	}
 }
 
 /**
