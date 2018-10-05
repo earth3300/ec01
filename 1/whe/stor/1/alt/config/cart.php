8176581767210d180f697051819dce72f35cbc87
Clarence Bos <wp@cbos.ca>
2018-10-05 10:48:54 -0400

    Update Store.

--- 1/whe/stor/1/alt/config/cart.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-( defined( 'SITE' ) || defined( 'WP_ADMIN' ) ) || exit;
-
-/** Cart Directory */
-define( 'SITE_CART_DIR', '/cart' );
-
-/** Cart Path */
-define( 'SITE_CART_PATH', SITE_THEME_HTML_PATH . SITE_CART_DIR );
