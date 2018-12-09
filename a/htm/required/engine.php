30a951a61134275fe8b7fd9c5e555525528624cc
Clarence Bos <wp@cbos.ca>
2018-12-09 11:44:57 -0500

    Update.

--- a/htm/required/engine.php
+++ a/htm/required/engine.php
@@ -641,6 +641,7 @@ class EC01HTML
     $str .= '</nav>' . PHP_EOL;
     $str .= '</div>' . PHP_EOL;
     $str .= '</footer>' . PHP_EOL;
+    
     /** Displays the time it took to generate the page. */
     $str .= SITE_ELAPSED_TIME ? get_site_elapsed() : '';
 
