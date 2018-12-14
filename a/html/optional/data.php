4d615e93cd84062c4c44589e20616859213c67a0
Clarence Bos <wp@cbos.ca>
2018-12-14 14:28:45 -0500

    Update.

--- a/html/optional/data.php
+++ a/html/optional/data.php
@@ -10,8 +10,8 @@
  *
  * File: data.php
  * Created: 2018-10-01
- * Update: 2018-11-06
- * Time: 07:41 EST
+ * Update: 2018-12-14
+ * Time: 14:15 EST
  */
 
 namespace Earth3300\EC01;
@@ -34,18 +34,18 @@ defined('NDA') || exit('NDA');
  */
 function get_tier_one_data()
 {
-	$items = [
-		'who' => [ 'name' => 'who' ],
-		'wha' => [ 'name' => 'what' ],
-		'whn' => [ 'name' => 'when' ],
-		'whe' => [ 'name' => 'where' ],
-		'how' => [ 'name' => 'how' ],
-		'why' => [ 'name' => 'why' ],
-		'wki' => [ 'name' => 'wiki' ],
-		'sto' => [ 'name' => 'store' ],
+  $items = [
+    'who' => [ 'name' => 'who' ],
+    'wha' => [ 'name' => 'what' ],
+    'whn' => [ 'name' => 'when' ],
+    'whe' => [ 'name' => 'where' ],
+    'how' => [ 'name' => 'how' ],
+    'why' => [ 'name' => 'why' ],
+    'wki' => [ 'name' => 'wiki' ],
+    'sto' => [ 'name' => 'store' ],
     'mem' => [ 'name' => 'members' ],
-		];
-	return $items;
+    ];
+  return $items;
 }
 
 /**
@@ -61,67 +61,67 @@ function get_tier_one_data()
  */
 function get_tier_two_data()
 {
-	$items = [
-		// whe (where)
-		'acad' => [ 'name' => 'academic' ],
-		'arts' => [ 'name' => 'arts' ],
-		'trad' => [ 'name' => 'trade' ],
-		'appl' => [ 'name' => 'applied' ],
-		'gard' => [ 'name' => 'gardening' ],
-		'care' => [ 'name' => 'care' ],
-		'cafe' => [ 'name' => 'cafe' ],
-		'moni' => [ 'name' => 'monitoring' ],
-		'anal' => [ 'name' => 'analysis' ],
-		'natu' => [ 'name' => 'nature' ],
+  $items = [
+    // whe (where)
+    'acad' => [ 'name' => 'academic' ],
+    'arts' => [ 'name' => 'arts' ],
+    'trad' => [ 'name' => 'trade' ],
+    'appl' => [ 'name' => 'applied' ],
+    'gard' => [ 'name' => 'gardening' ],
+    'care' => [ 'name' => 'care' ],
+    'cafe' => [ 'name' => 'cafe' ],
+    'moni' => [ 'name' => 'monitoring' ],
+    'anal' => [ 'name' => 'analysis' ],
+    'natu' => [ 'name' => 'nature' ],
 
-		// nature
-		'natu' => [ 'name' => 'nature' ],
+    // nature
+    'natu' => [ 'name' => 'nature' ],
 
-		// wha
-		'agri' => [ 'name' => 'agriculture' ],
-		'bldg' => [ 'name' => 'building' ],
-		'equi' => [ 'name' => 'equipment' ],
-		'supp' => [ 'name' => 'supply' ],
-		'prop' => [ 'name' => 'property' ],
-		'reso' => [ 'name' => 'resource' ],
-		'stru' => [ 'name' => 'structure' ],
-		'info' => [ 'name' => 'information' ],
-		'prod' => [ 'name' => 'product' ],
-		'desn' => [ 'name' => 'design' ],
+    // wha
+    'agri' => [ 'name' => 'agriculture' ],
+    'bldg' => [ 'name' => 'building' ],
+    'equi' => [ 'name' => 'equipment' ],
+    'supp' => [ 'name' => 'supply' ],
+    'prop' => [ 'name' => 'property' ],
+    'reso' => [ 'name' => 'resource' ],
+    'stru' => [ 'name' => 'structure' ],
+    'info' => [ 'name' => 'information' ],
+    'prod' => [ 'name' => 'product' ],
+    'desn' => [ 'name' => 'design' ],
 
-		// how
-		'stnd' => [ 'name' => 'standard' ],
-		'proc' => [ 'name' => 'process' ],
-		'mthd' => [ 'name' => 'method' ],
-		'proj' => [ 'name' => 'project' ],
-		'comm' => [ 'name' => 'communication' ],
-		'news' => [ 'name' => 'news' ],
-		'serv' => [ 'name' => 'service' ],
-		'whsl' => [ 'name' => 'wholesale' ],
-		'dsgn' => [ 'name' => 'design' ],
+    // how
+    'stnd' => [ 'name' => 'standard' ],
+    'proc' => [ 'name' => 'process' ],
+    'mthd' => [ 'name' => 'method' ],
+    'proj' => [ 'name' => 'project' ],
+    'comm' => [ 'name' => 'communication' ],
+    'news' => [ 'name' => 'news' ],
+    'serv' => [ 'name' => 'service' ],
+    'whsl' => [ 'name' => 'wholesale' ],
+    'dsgn' => [ 'name' => 'design' ],
     'powr' => [ 'name' => 'power' ],
 
-		// recycling
-		'reco' => [ 'name' => 'recovery' ],
+    // recycling
+    'reco' => [ 'name' => 'recovery' ],
 
-		// why
-		'ethc' => [ 'name' => 'ethics' ],
-		'sust' => [ 'name' => 'sustainability' ],
-		'reln' => [ 'name' => 'religion' ],
-		'motv' => [ 'name' => 'motivation' ],
+    // why
+    'ethc' => [ 'name' => 'ethics' ],
+    'sust' => [ 'name' => 'sustainability' ],
+    'reln' => [ 'name' => 'religion' ],
+    'motv' => [ 'name' => 'motivation' ],
 
-		// wki
-		'logc' => [ 'name' => 'logic' ],
-		'math' => [ 'name' => 'mathematics' ],
-		'comp' => [ 'name' => 'computing' ],
-		'phyc' => [ 'name' => 'physics' ],
+    // wki
+    'logc' => [ 'name' => 'logic' ],
+    'math' => [ 'name' => 'mathematics' ],
+    'comp' => [ 'name' => 'computing' ],
+    'phyc' => [ 'name' => 'physics' ],
 
-		'chem' => [ 'name' => 'chemistry' ],
-		'biol' => [ 'name' => 'biology' ],
-		'syst' => [ 'name' => 'systems' ],
-		'cogn' => [ 'name' => 'cognition' ],
-	];
-	return $items;
+    'chem' => [ 'name' => 'chemistry' ],
+    'biol' => [ 'name' => 'biology' ],
+    'syst' => [ 'name' => 'systems' ],
+    'cogn' => [ 'name' => 'cognition' ],
+  ];
+  return $items;
 }
 
 /**
@@ -135,109 +135,110 @@ function get_tier_two_data()
  */
 function get_tier_three_data()
 {
-	$items = [
-		'centr' => [ 'name' => 'center' ],
+  $items = [
+    'centr' => [ 'name' => 'center' ],
 
-		// Who
-		'chemi' => [ 'name' => 'chemistry', 'who' => 'chemist', ],
-		'cogni' => [ 'name' => 'cognition', 'who' => 'psychologist', ],
-		'cymat' => [ 'name' => 'cymatics', 'who' => 'cymaticist', ],
-		'genet' => [ 'name' => 'genetics', 'who' => 'geneticist', ],
-		'geolo' => [ 'name' => 'geology', 'who' => 'geologist', ],
-		'logic' => [ 'name' => 'logic', 'who' => 'logician', ],
-		'mathe' => [ 'name' => 'mathematics', 'who' => 'mathematician', ],
-		'physc' => [ 'name' => 'physics', 'who' => 'physicist', ],
+    // Who
+    'chemi' => [ 'name' => 'chemistry', 'who' => 'chemist', ],
+    'cogni' => [ 'name' => 'cognition', 'who' => 'psychologist', ],
+    'cymat' => [ 'name' => 'cymatics', 'who' => 'cymaticist', ],
+    'genet' => [ 'name' => 'genetics', 'who' => 'geneticist', ],
+    'geolo' => [ 'name' => 'geology', 'who' => 'geologist', ],
+    'logic' => [ 'name' => 'logic', 'who' => 'logician', ],
+    'mathe' => [ 'name' => 'mathematics', 'who' => 'mathematician', ],
+    'physc' => [ 'name' => 'physics', 'who' => 'physicist', ],
 
-		'archi' => [ 'name' => 'architecture', 'who' => 'architect', ],
-		'engin' => [ 'name' => 'engineering', 'who' => 'engineering', ],
-		'lands' => [ 'name' => 'landscaping', 'who' => 'landscaper', ],
-		'perma' => [ 'name' => 'permaculture', 'who' => 'permaculturist', ],
-		'progr' => [ 'name' => 'programming', 'who' => 'programmer', ],
-		'robot' => [ 'name' => 'robotics', 'who' => 'roboticist', ],
+    'archi' => [ 'name' => 'architecture', 'who' => 'architect', ],
+    'engin' => [ 'name' => 'engineering', 'who' => 'engineering', ],
+    'lands' => [ 'name' => 'landscaping', 'who' => 'landscaper', ],
+    'perma' => [ 'name' => 'permaculture', 'who' => 'permaculturist', ],
+    'progr' => [ 'name' => 'programming', 'who' => 'programmer', ],
+    'robot' => [ 'name' => 'robotics', 'who' => 'roboticist', ],
 
-		'music' => [ 'name' => 'music', 'who' => 'musician', ],
-		'paint' => [ 'name' => 'painting', 'who' => 'painter', ],
-		'percn' => [ 'name' => 'percussions', 'who' => 'percussionist', ],
-		'piano' => [ 'name' => 'piano', 'who' => 'pianist', ],
-		'pttry' => [ 'name' => 'pottery', 'who' => 'potter', ],
-		'saxop' => [ 'name' => 'saxophone', 'who' => 'saxophonist', ],
-		'sclpt' => [ 'name' => 'sculpture', 'who' => 'sculptor', ],
-		'violn' => [ 'name' => 'violin', 'who' => 'violinist', ],
+    'music' => [ 'name' => 'music', 'who' => 'musician', ],
+    'paint' => [ 'name' => 'painting', 'who' => 'painter', ],
+    'percn' => [ 'name' => 'percussions', 'who' => 'percussionist', ],
+    'piano' => [ 'name' => 'piano', 'who' => 'pianist', ],
+    'pttry' => [ 'name' => 'pottery', 'who' => 'potter', ],
+    'saxop' => [ 'name' => 'saxophone', 'who' => 'saxophonist', ],
+    'sclpt' => [ 'name' => 'sculpture', 'who' => 'sculptor', ],
+    'violn' => [ 'name' => 'violin', 'who' => 'violinist', ],
 
-		'bakng' => [ 'name' => 'baking', 'who' => '', ],
-		'brsta' => [ 'name' => 'barista', 'who' => 'barista', ],
-		'clnup' => [ 'name' => 'cleanup', 'who' => 'cleaner', ],
-		'cookg' => [ 'name' => 'cooking', 'who' => 'cook', ],
-		'recyl' => [ 'name' => 'recycling', 'who' => 'recycler', ],
-		'servg' => [ 'name' => 'serving', 'who' => 'server', ],
-		'storg' => [ 'name' => 'storage', 'who' => 'storage', ],
+    'bakng' => [ 'name' => 'baking', 'who' => '', ],
+    'brsta' => [ 'name' => 'barista', 'who' => 'barista', ],
+    'clnup' => [ 'name' => 'cleanup', 'who' => 'cleaner', ],
+    'cookg' => [ 'name' => 'cooking', 'who' => 'cook', ],
+    'recyl' => [ 'name' => 'recycling', 'who' => 'recycler', ],
+    'servg' => [ 'name' => 'serving', 'who' => 'server', ],
+    'storg' => [ 'name' => 'storage', 'who' => 'storage', ],
 
-		'bodyc' => [ 'name' => 'bodyc', 'who' => 'bodyc', ],
-		'cloth' => [ 'name' => 'clothing', 'who' => 'tailor', ],
-		'hairc' => [ 'name' => 'haircare', 'who' => 'hairdresser', ],
-		'nutri' => [ 'name' => 'nutrition', 'who' => 'nutritionist', ],
-		'physi' => [ 'name' => 'physio', 'who' => 'physiotherapist', ],
-		'psych' => [ 'name' => 'psyche', 'who' => 'pscyhologist', ],
-		'helth' => [ 'name' => 'health', 'who' => 'health-care', ],
-		'shoes' => [ 'name' => 'shoes', 'who' => 'shoemaker', ],
+    'bodyc' => [ 'name' => 'bodyc', 'who' => 'bodyc', ],
+    'cloth' => [ 'name' => 'clothing', 'who' => 'tailor', ],
+    'hairc' => [ 'name' => 'haircare', 'who' => 'hairdresser', ],
+    'nutri' => [ 'name' => 'nutrition', 'who' => 'nutritionist', ],
+    'physi' => [ 'name' => 'physio', 'who' => 'physiotherapist', ],
+    'psych' => [ 'name' => 'psyche', 'who' => 'pscyhologist', ],
+    'helth' => [ 'name' => 'health', 'who' => 'health-care', ],
+    'shoes' => [ 'name' => 'shoes', 'who' => 'shoemaker', ],
 
-		'culti' => [ 'name' => 'cultivating', 'who' => 'cultivator', ],
-		'hrvtg' => [ 'name' => 'harvesting', 'who' => 'harvester', ],
-		'plntg' => [ 'name' => 'planting', 'who' => 'planter', ],
-		'prepg' => [ 'name' => 'preparing', 'who' => 'preparer', ],
-		'procg' => [ 'name' => 'processing', 'who' => 'processer', ],
-		'weedg' => [ 'name' => 'weeding', 'who' => 'weeder', ],
+    'culti' => [ 'name' => 'cultivating', 'who' => 'cultivator', ],
+    'hrvtg' => [ 'name' => 'harvesting', 'who' => 'harvester', ],
+    'plntg' => [ 'name' => 'planting', 'who' => 'planter', ],
+    'prepg' => [ 'name' => 'preparing', 'who' => 'preparer', ],
+    'procg' => [ 'name' => 'processing', 'who' => 'processer', ],
+    'weedg' => [ 'name' => 'weeding', 'who' => 'weeder', ],
 
-		'anlys' => [ 'name' => 'analysis', 'who' => 'analyst', ],
-		'audio' => [ 'name' => 'audio', 'who' => 'audio-engineer', ],
-		'photo' => [ 'name' => 'photography', 'who' => 'photographer', ],
-		'presg' => [ 'name' => 'presenting', 'who' => 'presenter', ],
-		'video' => [ 'name' => 'video', 'who' => 'videographer', ],
-		'writg' => [ 'name' => 'writing', 'who' => 'writer', ],
-		'grphc' => [ 'name' => 'graphics', 'who' => 'graphics-designer', ],
+    'anlys' => [ 'name' => 'analysis', 'who' => 'analyst', ],
+    'audio' => [ 'name' => 'audio', 'who' => 'audio-engineer', ],
+    'photo' => [ 'name' => 'photography', 'who' => 'photographer', ],
+    'presg' => [ 'name' => 'presenting', 'who' => 'presenter', ],
+    'video' => [ 'name' => 'video', 'who' => 'videographer', ],
+    'writg' => [ 'name' => 'writing', 'who' => 'writer', ],
+    'grphc' => [ 'name' => 'graphics', 'who' => 'graphics-designer', ],
 
-		'carpt' => [ 'name' => 'carpentry', 'who' => 'carpenter', ],
-		'drywl' => [ 'name' => 'drywall', 'who' => 'drywaller', ],
-		'elect' => [ 'name' => 'electrical', 'who' => 'electrician', ],
-		'mason' => [ 'name' => 'masonry', 'who' => 'mason', ],
-		'mecha' => [ 'name' => 'mechanical', 'who' => 'mechanic', ],
-		'plumb' => [ 'name' => 'plumbing', 'who' => 'plumber', ],
-		'weldg' => [ 'name' => 'welding', 'who' => 'welder', ],
-		'roofg' => [ 'name' => 'roofing', 'who' => 'roofer', ],
+    'carpt' => [ 'name' => 'carpentry', 'who' => 'carpenter', ],
+    'drywl' => [ 'name' => 'drywall', 'who' => 'drywaller', ],
+    'elect' => [ 'name' => 'electrical', 'who' => 'electrician', ],
+    'mason' => [ 'name' => 'masonry', 'who' => 'mason', ],
+    'mecha' => [ 'name' => 'mechanical', 'who' => 'mechanic', ],
+    'plumb' => [ 'name' => 'plumbing', 'who' => 'plumber', ],
+    'weldg' => [ 'name' => 'welding', 'who' => 'welder', ],
+    'roofg' => [ 'name' => 'roofing', 'who' => 'roofer', ],
 
-		// Wha (What)
-		'geolo' => [ 'name' => 'geology', 'who' => 'geologist', ],
-		'miner' => [ 'name' => 'mineral', 'who' => 'geologist', ],
-		'strat' => [ 'name' => 'strata', 'who' => 'geologist', ],
-		'terrn' => [ 'name' => 'terrain', 'who' => 'geologist', ],
-		'water' => [ 'name' => 'water', 'who' => 'hydrologist', ],
-		'atmos' => [ 'name' => 'atmosphere', 'who' => 'meterologist', ],
-		'climt' => [ 'name' => 'climate', 'who' => 'meterologist', ],
-		'wther' => [ 'name' => 'weather', 'who' => 'meterologist', ],
-		'lakes' => [ 'name' => 'lakes', 'who' => '', ],
-		'ecosy' => [ 'name' => 'ecosystem', 'who' => '', ],
-		'plant' => [ 'name' => 'plant', 'who' => '', ],
-		'insct' => [ 'name' => 'insect', 'who' => '', ],
-		'animl' => [ 'name' => 'animal', 'who' => '', ],
+    // Wha (What)
+    'geolo' => [ 'name' => 'geology', 'who' => 'geologist', ],
+    'miner' => [ 'name' => 'mineral', 'who' => 'geologist', ],
+    'strat' => [ 'name' => 'strata', 'who' => 'geologist', ],
+    'terrn' => [ 'name' => 'terrain', 'who' => 'geologist', ],
+    'water' => [ 'name' => 'water', 'who' => 'hydrologist', ],
+    'atmos' => [ 'name' => 'atmosphere', 'who' => 'meterologist', ],
+    'climt' => [ 'name' => 'climate', 'who' => 'meterologist', ],
+    'wther' => [ 'name' => 'weather', 'who' => 'meterologist', ],
+    'lakes' => [ 'name' => 'lakes', 'who' => '', ],
+    'ecosy' => [ 'name' => 'ecosystem', 'who' => '', ],
+    'plant' => [ 'name' => 'plant', 'who' => '', ],
+    'insct' => [ 'name' => 'insect', 'who' => '', ],
+    'animl' => [ 'name' => 'animal', 'who' => '', ],
 
-		'packs' => [ 'name' => 'packs', 'who' => 'tailor', ],
-		'shelt' => [ 'name' => 'shelter', 'who' => 'carpenter', ],
-		'sleep' => [ 'name' => 'sleep', 'who' => 'carpenter', ],
-		'dwell' => [ 'name' => 'dwelling', 'who' => 'builder', ],
-		'wkshp' => [ 'name' => 'workshop', 'who' => 'builder', ],
+    'packs' => [ 'name' => 'packs', 'who' => 'tailor', ],
+    'shelt' => [ 'name' => 'shelter', 'who' => 'carpenter', ],
+    'sleep' => [ 'name' => 'sleep', 'who' => 'carpenter', ],
+    'dwell' => [ 'name' => 'dwelling', 'who' => 'builder', ],
+    'wkshp' => [ 'name' => 'workshop', 'who' => 'builder', ],
+    'compu' => [ 'name' => 'computing', 'who' => 'programmer', ],
 
-		'agric' => [ 'name' => 'agricultural', 'who' => 'farmer', ],
-		'mecha' => [ 'name' => 'mechanical', 'who' => 'mechanic', ],
-		'elect' => [ 'name' => 'electrical', 'who' => 'electrician', ],
-		'gener' => [ 'name' => 'generator', 'who' => 'electrician', ],
+    'agric' => [ 'name' => 'agricultural', 'who' => 'farmer', ],
+    'mecha' => [ 'name' => 'mechanical', 'who' => 'mechanic', ],
+    'elect' => [ 'name' => 'electrical', 'who' => 'electrician', ],
+    'gener' => [ 'name' => 'generator', 'who' => 'electrician', ],
 
-		'fastn' => [ 'name' => 'fasteners', 'who' => 'fasteners', ],
-		'sheet' => [ 'name' => 'sheet-goods', 'who' => 'sheet-goods', ],
-		'struc' => [ 'name' => 'structural', 'who' => 'structural', ],
+    'fastn' => [ 'name' => 'fasteners', 'who' => 'fasteners', ],
+    'sheet' => [ 'name' => 'sheet-goods', 'who' => 'sheet-goods', ],
+    'struc' => [ 'name' => 'structural', 'who' => 'structural', ],
 
-		// How (How)
-		'softw' => [ 'name' => 'software', 'who' => 'programmer', ],
-		'grwth' => [ 'name' => 'growth', 'who' => 'planner', ],
-	];
-	return $items;
+    // How (How)
+    'softw' => [ 'name' => 'software', 'who' => 'programmer', ],
+    'grwth' => [ 'name' => 'growth', 'who' => 'planner', ],
+  ];
+  return $items;
 }
